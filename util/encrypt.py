4d59f7d267d6e61a1c79ebb97e4103c9fb34cb02
Thiago Nóbrega <thiagonobrega@gmail.com>
2017-11-02 15:59:01 -0300

    Now we can execute the BAP with bigramas or words...

--- util/encrypt.py
+++ util/encrypt.py
@@ -8,7 +8,7 @@ import ngram
 from lib.mybloom.bloomfilter import BloomFilter
 
 
-def encryptData(data,size,fp=0.01,n=2):
+def encryptDataBigram(data,size,fp=0.01,n=2):
     """
         n : 2 = Bigrams
         size : Size of BF
@@ -20,5 +20,20 @@ def encryptData(data,size,fp=0.01,n=2):
                     
     for bigram in bigrams:
         bloomfilter.add(str(bigram))
+                    
+    return bloomfilter
+
+def encryptDataWords(data,size,fp=0.01):
+    """
+        n : 2 = Bigrams
+        size : Size of BF
+        fp : False positive rate
+    """
+    bloomfilter = BloomFilter(size,fp)
+    #index = ngram.NGram(N=n)
+    #bigrams = list(index.ngrams(index.pad(str(data))))
+                    
+    for word in data.split():
+        bloomfilter.add(str(word))
                     
     return bloomfilter
